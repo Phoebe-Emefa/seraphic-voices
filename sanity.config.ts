829e4ad830373526e9fd0651a7a3ca508fab67b9
Phoebe-Emefa <phoebeemefa@gmail.com>
2023-09-20 22:47:50 +0000

    updates

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,15 +2,11 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import schemas from "./sanity/schemas";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION;
-
 const config = defineConfig({
-  projectId: projectId as string,
-  dataset: dataset as string,
+  projectId: "5xuvntt7",
+  dataset: "production",
   title: "Seraphic Voices",
-  apiVersion: apiVersion as string,
+  apiVersion: "2023-09-20",
   basePath: "/admin",
   plugins: [deskTool()],
   schema: {
