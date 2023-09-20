f6621c825af7641c8c9ded901cce608a266de87d
Phoebe-Emefa <phoebeemefa@gmail.com>
2023-09-20 22:39:12 +0000

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,6 @@ import Container from '@/components/container';
 import { Metadata } from 'next';
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 
-export const metadata: Metadata = {
-  title: 'Seraphic Vocies of Toronto',
-  description: `Seraphic Voices of Toronto`,
-  openGraph: {
-    images: ['/images/seraphic-voices.png']
-  }
-};
 
 export default function RootLayout({
   children
